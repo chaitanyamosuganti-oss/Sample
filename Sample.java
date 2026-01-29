1f84cb4aa44373f9dcef9930e642aa9de4bbe97e
chaitanyamosuganti-oss <chaitanya.mosuganti@gmail.com>
2026-01-29 11:05:50 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Sample{
 public static void main(String args[]){
 System.out.print("To check given number is Prime or Not");
-int n,i,count;
+int n,i,count=0;
 Scanner s1=new Scanner(System.in);
 System.out.println("enter any number");
 n=s1.nextInt();
