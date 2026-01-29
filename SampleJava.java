38ea46d68d8a0d88abccf2564764a14489875c7c
MchaitanyaAshok <chaitanyamosuganti@kluniversity.in>
2026-01-29 11:00:50 +0530

    updated with prime number program

--- SampleJava.java
+++ SampleJava.java
@@ -1,5 +1,19 @@
+import java.util.*;
 class Sample{
 public static void main(String args[]){
-System.out.print("Hello");
+System.out.print("To check given number is Prime or Not");
+int n,i,count;
+Scanner s1=new Scanner(System.in);
+System.out.println("enter any number");
+n=s1.nextInt();
+for (i=2;i<n;i++)
+{
+	if(n%i==0)
+		count++;
+}
+if(count==0)
+	Sytem.out.println(n+"	is a prime number");
+else
+	System.out.println(n+"	is not a prime number");
 }
 }
